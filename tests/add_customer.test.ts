ea5403e33cef6e811820d28c82ce07c20cb4d541
r3son4nc3 <onetenstv@gmail.com>
2023-12-08 01:03:59 +0200

    init autotests, add github actions

---
+++ tests/add_customer.test.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+console.log('Test case: add new customer');
+
+test('test', async ({ page }) => {
+  await page.goto('https://material-kit-pro-react.devias.io/dashboard/customers');
+  await page.goto('https://material-kit-pro-react.devias.io/auth/amplify/login?returnTo=%2Fdashboard%2Fcustomers');
+  await page.getByRole('button', { name: 'Consent' }).click();
+  await page.getByRole('button', { name: 'Add' }).click();
+  await page.getByLabel('Full name *').click();
+  await page.getByLabel('Full name *').fill('Alex Garcia');
+  await page.getByLabel('Email address *').click();
+  await page.getByLabel('Email address *').fill('alexgarcia@mail.com');
+   
+  await page.getByRole('button', { name: 'Save' }).click();
+  
+  await page.locator('div').filter({ hasText: 'Customer created' }).nth(3).click();
+});
---
+++ tests/update_customer.test.ts
@@ -0,0 +1,27 @@
+import { test, expect } from "@playwright/test";
+
+console.log("Test case: update customer");
+
+test("test", async ({ page }) => {
+  await page.goto(
+    "https://material-kit-pro-react.devias.io/dashboard/customers"
+  );
+  await page.goto(
+    "https://material-kit-pro-react.devias.io/auth/amplify/login?returnTo=%2Fdashboard%2Fcustomers"
+  );
+  await page.getByRole("button", { name: "Log In" }).click();
+  await page.getByRole("button", { name: "Consent" }).click();
+  await page.getByRole("link", { name: "Carson Darrin" }).click();
+  await page.getByRole("main").getByRole("link", { name: "Edit" }).click();
+  await page.getByLabel("Full name *").click();
+
+  await page.getByLabel("Full name *").fill("Barry Sheppard");
+  await expect(page.getByRole("button", { name: "Update" })).toBeVisible();
+  await page.getByRole("button", { name: "Update" }).click();
+
+  await page
+    .locator("div")
+    .filter({ hasText: "Customer updated" })
+    .nth(3)
+    .focus({ timeout: 1000 });
+});
